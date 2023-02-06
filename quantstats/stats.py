ff9d3c0f05f6d525252ba8cdbdcf2b7f0bccb468
Pavel Reidman <preidman@gmail.com>
2023-02-06 14:24:45 +0400

    5

--- quantstats/stats.py
+++ quantstats/stats.py
@@ -731,11 +731,11 @@ def recovery_factor(returns, prepare_returns=True):
         returns = _utils._prepare_returns(returns)
     # total_returns = comp(returns)
     max_dd = max_drawdown(returns)
-    print(returns["returns"].cumsum().tail(1))
-    print(abs(max_dd))
-    print(returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns))
-    print((returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns)).value)
-    print(type(returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns)))
+    # print(returns["returns"].cumsum().tail(1))
+    print(type(abs(max_dd)))
+    # print(returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns))
+    # print((returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns)).value)
+    # print(type(returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns)))
 
     return (returns["returns"].cumsum().tail(1) / abs(max_dd) / len(returns)).value
 
